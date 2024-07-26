e8c6cfbbb6790305af2f8771de0523ccbc3bf14e
gamel1ke <caneiyrxekphph@gmail.com>
2024-07-26 16:21:15 +0700

    รับเงินหมื่น

---
+++ ep2 E3/indax.ts
@@ -0,0 +1,33 @@
+function add(a: number, b: number){
+    return  a + b
+}
+
+function remove(a: number, b: number){
+    return a-b
+}
+
+function mul(a: number, b: number, c:number){
+    return a*b*c
+}
+
+console.log(remove(6,11))
+
+console.log(mul(6,11,16))
+
+function grade(homework: number, midterm: number ,final: number){
+    let score: number = homework + midterm + final
+
+    if (score < 50) {
+       return 'เกรด F'
+   } else if (score < 60) {  
+       return 'เกรด D'
+   } else if  (score < 70){ 
+       return 'เกรด C'
+   } else if  (score < 80){
+       return 'เกรด B'
+   } else if  (score >=80){
+       return 'เกรด A' 
+   }
+       
+}
+console.log(grade(50, 30, 20))
---
+++ ep3 ni/index.ts
@@ -0,0 +1,41 @@
+function helloworld(){
+    console.log("สวัดดี")
+}
+
+helloworld() // ไม่มี input ไม่มี output
+
+function hellowName(name: string){
+    console.log (name)
+}
+hellowName('game')
+
+function getPi(){
+    return 3.14
+
+console.log    
+
+function st(ifname: same: string) {
+    if(fname=='mix'&&snme==='zane')
+    console.log('พร้องเรียน')
+    }else {
+    console.log('ยังไม่พร้อม')
+    }
+}
+
+st('mix,zane')
+
+function st(sex: string, Height: number, Weight: number) {
+    if (sex === 'man' && (Height > 170 || Weight > 50 && Weight << 110)){
+        console.log('จับใบดำใบแดง')
+    } else {
+        console.log('ไม่เข้าเกณฑ์')
+    }
+}st('man', 160 , 250
+
+ function st(age: number, salary: number, deposit: number) {
+    if (age > 16 && salary < 70000 && deposit < 500000) {
+        console.log('เอาไปเลยหนึ่งหมื่นบาท')
+    } else {
+        console.log('ไม่ให้หรอก')
+    }
+} st(69, 15000, 200000)
